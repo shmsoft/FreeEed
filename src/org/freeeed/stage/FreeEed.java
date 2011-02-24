5bdcae0b508e4c3c44007267d366fe9d6ba49aa9
Mark Kerzner <markkerzner@gmail.com>
2011-02-23 23:15:43 -0600

    First checkin

---
+++ src/org/freeeed/stage/FreeEed.java
@@ -0,0 +1,14 @@
+package org.freeeed.stage;
+
+public class FreeEed {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    FreeEed instance = new FreeEed(args);
+  }
+
+  public FreeEed(String[] args) {
+  }
+}
