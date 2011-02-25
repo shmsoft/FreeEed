5cbc545e872c1af55966a7930b1e84564dd81829
Mark Kerzner <markkerzner@gmail.com>
2011-02-25 00:11:23 -0600

    First checkin

--- src/org/freeeed/stage/FreeEed.java
+++ src/org/freeeed/stage/FreeEed.java
@@ -8,40 +8,43 @@ import org.apache.commons.cli.ParseException;
 
 public class FreeEed {
 
-  private Options options;
+	private Options options;
 
-  /**
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
-    FreeEed instance = new FreeEed(args);
-  }
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		FreeEed instance = new FreeEed(args);
+	}
 
-  public FreeEed(String[] args) {
-    processOptions(args);
-  }
+	public FreeEed(String[] args) {
+		processOptions(args);
+	}
 
-  private void processOptions(String[] args) {
-    try {
-      options = new Options();
-      options.addOption("h", false, "Print help for this application");
-      options.addOption("u", true, "The username to use");
-      options.addOption("dsn", true, "The data source to use");
+	private void processOptions(String[] args) {
+		try {
+			options = new Options();
+			options.addOption("h", false, "Print help for FreeEed");
+			options.addOption("stage", false, "Perform staging");
+			options.addOption("par", true, "File with parameters for processing");
+			options.addOption("in", true, "Input directory to stage");
 
-      BasicParser parser = new BasicParser();
-      CommandLine cl = parser.parse(options, args);
+			BasicParser parser = new BasicParser();
+			CommandLine cl = parser.parse(options, args);
 
-      if (cl.hasOption('h')) {
-        HelpFormatter f = new HelpFormatter();
-        f.printHelp("OptionsTip", options);
-      } else {
-        System.out.println(cl.getOptionValue("u"));
-        System.out.println(cl.getOptionValue("dsn"));
-      }
-    } catch (ParseException e) {
-      // TODO use logging
-      e.printStackTrace(System.out);
+			if (cl.hasOption('h') || cl.getOptions().length == 0) {
+				HelpFormatter f = new HelpFormatter();
+				f.printHelp("How to use FreeEed", options);
+			} else {
+				String [] in = cl.getOptionValues("in");	
+				for (String i: in) {
+					System.out.println(i);
+				}
+			}
+		} catch (ParseException e) {
+			// TODO use logging
+			e.printStackTrace(System.out);
 
-    }
-  }
+		}
+	}
 }
