8d7b9b2d91c9a5e1621b0f8640526beea4d912f8
Mark Kerzner <markkerzner@gmail.com>
2011-08-09 11:55:51 -0500

    Refactor

--- src/org/freeeed/main/Version.java
+++ src/org/freeeed/main/Version.java
@@ -7,7 +7,7 @@ package org.freeeed.main;
 public class Version {
 
     public static String getVersion() {
-        return "FreeEed V2.5.10";
+        return "FreeEed V2.6.1";
     }
 
     public static String getSupportEmail() {
