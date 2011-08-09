4b5b7a682a72386d26b78c6dbb5cb260be04ff4a
Mark Kerzner <markkerzner@gmail.com>
2011-08-09 12:50:17 -0500

    Move package

---
+++ src/org/freeeed/common/History.java
@@ -0,0 +1,78 @@
+package org.freeeed.common;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.freeeed.main.FreeEedLogging;
+import org.freeeed.main.LinuxUtil;
+import org.freeeed.main.Util;
+
+/**
+ *
+ * @author mark
+ */
+public class History {
+
+    private static String historyFileName = FreeEedLogging.history;
+    private static History instance = new History();
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd HH:mm:ss   ");
+    private long historyLastModified = 0;
+    
+    private History() {
+        // singleton
+    }
+
+    public static History getInstance() {
+        return instance;
+    }
+    
+    public boolean isHistoryNew() {
+        long lastModified = new File(historyFileName).lastModified();
+        if (lastModified != historyLastModified) {
+            historyLastModified = lastModified;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public String getHistory() throws Exception {
+        String history = "";
+        checkHistoryFile();
+        history = Util.readTextFile(historyFileName);
+        return history;
+    }
+
+    /**
+     * Create the file if it is not there
+     */
+    synchronized private void checkHistoryFile() throws Exception {
+        if (!new File(historyFileName).exists()) {
+            Util.writeTextFile(historyFileName, getFormattedDate() + "History started");
+        }
+    }
+
+    private String getFormattedDate() {
+        return sdf.format(new Date());
+    }
+
+    synchronized public void eraseHistory() throws Exception {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyMMdd_HHmmss");
+        String command = "cp " + historyFileName + " "
+                + historyFileName + "." + dateFormat.format(new Date());
+        LinuxUtil.runLinuxCommand(command);
+        new File(historyFileName).delete();
+        checkHistoryFile();
+    }
+
+    static synchronized public void appendToHistory(String moreHistory) {
+        try {
+            getInstance().doAppendToHistory(moreHistory);
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    synchronized private void doAppendToHistory(String moreHistory) throws Exception {
+        Util.appendToTextFile(historyFileName, getFormattedDate() + moreHistory + Util.NL);
+    }
+}
---
+++ src/org/freeeed/common/Review.java
@@ -0,0 +1,40 @@
+package org.freeeed.common;
+
+import com.google.common.io.Files;
+import java.io.File;
+import java.io.IOException;
+import org.freeeed.main.FreeEedException;
+import org.freeeed.main.ParameterProcessing;
+
+/**
+ *
+ * @author mark
+ */
+public class Review {
+
+	public static void deliverFiles() throws IOException, FreeEedException {
+		File outputFolder = new File(ParameterProcessing.OUTPUT_DIR + "/output");
+		// if I have a "part...." file there, rename it to output.csv
+		File[] files = outputFolder.listFiles();
+		if (files == null) {
+			throw new FreeEedException("No results yet");
+		}
+		boolean success = false;
+		for (File file : files) {
+			if (file.getName().startsWith("_SUCCESS")) {
+				success = true;
+			}
+		}
+		if (!success) {
+			throw new FreeEedException("No results yet");
+		}
+		for (File file : files) {
+			if (file.getName().startsWith("part")) {
+				Files.move(file, new File(outputFolder.getPath() + "/metadata.csv"));
+			}
+		}
+		if (Stats.getInstance().getStatsFile().exists()) {
+			Files.move(Stats.getInstance().getStatsFile(), new File(outputFolder.getPath() + "/report.txt"));
+		}
+	}
+}
