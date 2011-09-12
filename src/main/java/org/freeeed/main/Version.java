4c17874f4f55d436f05ac1e358f8de9d69cad92a
Mark Kerzner <markkerzner@gmail.com>
2011-09-12 17:56:25 -0500

    change version for maven build

--- src/main/java/org/freeeed/main/Version.java
+++ src/main/java/org/freeeed/main/Version.java
@@ -7,7 +7,7 @@ package org.freeeed.main;
 public class Version {
 
     public static String getVersion() {
-        return "FreeEed V2.8.6";
+        return "FreeEed V2.9.0";
     }
 
     public static String getSupportEmail() {
