40290626cdc9010dcdc74d9ad486376d0bb37a67
Mark Kerzner <markkerzner@gmail.com>
2011-10-09 14:25:31 -0500

    fixed build with java-libpst

--- src/main/java/org/freeeed/main/Version.java
+++ src/main/java/org/freeeed/main/Version.java
@@ -7,7 +7,7 @@ package org.freeeed.main;
 public class Version {
 
     public static String getVersion() {
-        return "FreeEed V2.9.1";
+        return "FreeEed V2.9.2";
     }
 
     public static String getSupportEmail() {
