85bc7cf35c4ff07b7ed7e659abc64541613ebe9d
Mark Kerzner <markkerzner@gmail.com>
2012-01-04 23:58:11 -0600

    Again, don't save attachments separately

--- src/main/java/org/freeeed/mail/EmlParser.java
+++ src/main/java/org/freeeed/mail/EmlParser.java
@@ -3,24 +3,29 @@ package org.freeeed.mail;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.Session;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 
 public class EmlParser {
+
     private File emailFile;
-    private ArrayList <String> to;
-    
+    private ArrayList<String> to;
+    private MimeMessage email;
+
     public EmlParser(File emailFile) {
         this.emailFile = emailFile;
         parseEmail();
     }
+
     private void parseEmail() {
         java.util.Properties properties = System.getProperties();
         Session session = Session.getDefaultInstance(properties);
-        MimeMessage email = null;
         try {
             FileInputStream fis = new FileInputStream(emailFile);
             email = new MimeMessage(session, fis);
@@ -35,10 +40,16 @@ public class EmlParser {
     }
 
     public static void main(String argv[]) {
-        EmlParser instance = new EmlParser(new File("test-data/jpst/2147"));
-        ArrayList <String> to = instance.getTo();
-        for (String t: to) {
+        EmlParser instance = new EmlParser(new File("test-data/jpst/2147.eml"));
+        ArrayList<String> to = instance.getTo();
+        for (String t : to) {
             System.out.println(t);
+        }
+        instance = new EmlParser(new File("test-data/jpst/802.eml"));
+        try {
+            instance.saveAttachments();
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
         }
     }
 
@@ -59,14 +70,37 @@ public class EmlParser {
     /**
      * @param to the to to set
      */
-    public void setTo(ArrayList <String> to) {
+    public void setTo(ArrayList<String> to) {
         this.to = to;
     }
 
     /**
      * @return the to
      */
-    public ArrayList <String> getTo() {
+    public ArrayList<String> getTo() {
         return to;
+    }
+
+    public void saveAttachments() throws MessagingException, IOException {       
+        if (email.isMimeType("text/*")) {
+            String s = (String) email.getContent();
+        }
+        
+        if (email.isMimeType("multipart/alternative")) {
+            Multipart mp = (Multipart) email.getContent();
+            String text = null;
+            for (int i = 0; i < mp.getCount(); i++) {
+                MimeBodyPart bodyPart = (MimeBodyPart) mp.getBodyPart(i);
+                String attachmentFileName = Integer.toString(i);
+                bodyPart.saveFile(attachmentFileName);                
+            }
+        } else if (email.isMimeType("multipart/*")) {
+            Multipart mp = (Multipart) email.getContent();
+            for (int i = 0; i < mp.getCount(); i++) {
+                MimeBodyPart bodyPart = (MimeBodyPart) mp.getBodyPart(i);
+                String attachmentFileName = Integer.toString(i);
+                bodyPart.saveFile(attachmentFileName);    
+            }
+        }
     }
 }
