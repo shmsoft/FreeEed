5a6bb61ff187f1516cb7ddda4b9f5208b48c9a17
Mark Kerzner <markkerzner@gmail.com>
2012-02-16 23:06:26 -0600

    options dialog

--- src/main/java/org/freeeed/main/Version.java
+++ src/main/java/org/freeeed/main/Version.java
@@ -8,7 +8,7 @@ import java.util.Date;
  * @author mark
  */
 public class Version {
-    public static final String version = "FreeEed V3.2.2";
+    public static final String version = "FreeEed V3.2.3";
     
     public static String getVersionAndBuild() {
         return version
