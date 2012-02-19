ca738992eef31e736299b0529999ab56e8f29974
Mark Kerzner <markkerzner@gmail.com>
2012-02-19 15:56:51 -0600

    File size limit 1 Gig, 2 Gig for java

--- run_gui.bat
+++ run_gui.bat
@@ -1,1 +1,1 @@
-java -cp target/FreeEed-1.0-SNAPSHOT-jar-with-dependencies.jar;drivers/truezip-driver-zip-7.3.4.jar org.freeeed.ui.FreeEedUI
+java -Xmx2048m -cp target/FreeEed-1.0-SNAPSHOT-jar-with-dependencies.jar;drivers/truezip-driver-zip-7.3.4.jar org.freeeed.ui.FreeEedUI
