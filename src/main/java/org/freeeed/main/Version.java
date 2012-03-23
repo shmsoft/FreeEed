d35b237c0e523a04f3b371ccf6a196e6674d0896
Administrator <Administrator@ip-0A503F89.ec2.internal>
2012-03-22 19:39:47 -0500

    set field separator in Windows processing

--- src/main/java/org/freeeed/main/Version.java
+++ src/main/java/org/freeeed/main/Version.java
@@ -8,7 +8,7 @@ import java.util.Date;
  * @author mark
  */
 public class Version {
-    public static final String version = "FreeEed V3.5.9";
+    public static final String version = "FreeEed V3.6.0";
     
     public static String getVersionAndBuild() {
         return version
