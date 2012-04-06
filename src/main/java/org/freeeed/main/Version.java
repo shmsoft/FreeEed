a99ab5ff3c8e7d816c885d691d7b54dd557dfb94
Mark Kerzner <markkerzner@gmail.com>
2012-04-05 23:37:26 -0500

    version

--- src/main/java/org/freeeed/main/Version.java
+++ src/main/java/org/freeeed/main/Version.java
@@ -8,7 +8,7 @@ import java.util.Date;
  * @author mark
  */
 public class Version {
-    public static final String version = "FreeEed V3.6.1";
+    public static final String version = "FreeEed V3.6.2";
     
     public static String getVersionAndBuild() {
         return version
