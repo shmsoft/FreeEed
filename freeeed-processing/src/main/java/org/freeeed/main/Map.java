c4012532696bbce77daea72f519a6a35329e615c
ilazarov <ivan_lazarov@gbg.bg>
2014-01-01 21:38:33 +0200

    Map will call PstProcessor directly in case of S3 downloaded pst file

--- freeeed-processing/src/main/java/org/freeeed/main/Map.java
+++ freeeed-processing/src/main/java/org/freeeed/main/Map.java
@@ -21,6 +21,8 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.List;
 
+import javax.rmi.CORBA.Util;
+
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.MD5Hash;
 import org.apache.hadoop.io.MapWritable;
@@ -35,6 +37,8 @@ import org.freeeed.print.OfficePrint;
 import org.freeeed.services.Project;
 import org.freeeed.services.Settings;
 import org.freeeed.services.Stats;
+
+
 
 
 
@@ -85,10 +89,12 @@ public class Map extends Mapper<LongWritable, Text, MD5Hash, MapWritable> {
         Stats.getInstance().setZipFileName(zipFile);
         
         project.setupCurrentCustodianFromFilename(zipFile);
+        logger.info("Will use current custodian: {}", project.getCurrentCustodian());
         // if we are in Hadoop, copy to local tmp         
         if (project.isEnvHadoop()) {
+            String extension = org.freeeed.services.Util.getExtension(zipFile);
             String tmpDir = ParameterProcessing.TMP_DIR_HADOOP;
-            File tempZip = File.createTempFile("freeeed", ".zip", new File(tmpDir));
+            File tempZip = File.createTempFile("freeeed", ("." + extension), new File(tmpDir));
             tempZip.delete();
             if (project.isFsHdfs() || project.isFsLocal()) {
                 String cmd = "hadoop fs -copyToLocal " + zipFile + " " + tempZip.getPath();
@@ -100,9 +106,19 @@ public class Map extends Mapper<LongWritable, Text, MD5Hash, MapWritable> {
             
             zipFile = tempZip.getPath();
         }
-        // process archive file
-        ZipFileProcessor processor = new ZipFileProcessor(zipFile, context, luceneIndex);
-        processor.process(false, null);
+        
+        if (PstProcessor.isPST(zipFile)) {
+            try {
+                new PstProcessor(zipFile, context, luceneIndex).process();
+            } catch (Exception e) {
+                logger.error("Problem with PST processing...", e);
+            }
+        } else {
+            logger.info("Will create Zip File processor for: {}", zipFile);
+            // process archive file
+            ZipFileProcessor processor = new ZipFileProcessor(zipFile, context, luceneIndex);
+            processor.process(false, null);
+        }
     }
 
     // TODO move indexing to reducer
