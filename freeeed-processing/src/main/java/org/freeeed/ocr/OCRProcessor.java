477299f1ad5aeeb3c8252ca111ce243c1bb4d691
ilazarov <ivan_lazarov@gbg.bg>
2014-06-19 15:14:35 +0300

    OCR timeout when waiting for result

--- freeeed-processing/src/main/java/org/freeeed/ocr/OCRProcessor.java
+++ freeeed-processing/src/main/java/org/freeeed/ocr/OCRProcessor.java
@@ -23,6 +23,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.freeeed.ocr.tess.TesseractOCRFactory;
@@ -80,7 +81,7 @@ public class OCRProcessor {
             
             for (Future<String> fResult : fResults) {
                 try {
-                    String text = fResult.get();
+                    String text = fResult.get(10000, TimeUnit.MILLISECONDS);
                     if (text != null) {
                         imageTexts.add(text);
                     }
