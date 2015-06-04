088ba175fcfd649b5a2bf7db5b6af781da3fc5ed
Mark Kerzner <mark@elephantscale.com>
2015-06-03 20:00:46 -0500

    format

--- freeeed-processing/src/main/java/org/freeeed/mail/EmlParser.java
+++ freeeed-processing/src/main/java/org/freeeed/mail/EmlParser.java
@@ -13,7 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 package org.freeeed.mail;
 
 import java.io.File;
@@ -45,8 +45,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EmlParser implements EmailDataProvider {
+
     private static final Logger log = LoggerFactory.getLogger(EmlParser.class);
-    
+
     private File emailFile;
     private ArrayList<String> to;
     private Address[] _bcc;
@@ -56,10 +57,10 @@ public class EmlParser implements EmailDataProvider {
     private String _subject;
     private Object _content;
     private MimeMessage email;
-    private List<String> _attachments;
+    private final List<String> _attachments;
     private Date _date;
     private Date _sentDate;
-    private Map<String, String> attachmentsContent;
+    private final Map<String, String> attachmentsContent;
     private int attachmentSeq = 0;
 
     public EmlParser(File emailFile) throws Exception {
@@ -68,14 +69,14 @@ public class EmlParser implements EmailDataProvider {
         System.setProperty("mail.mime.address.strict", "false");
         System.setProperty("mail.mime.decodeparameters", "true");
         attachmentsContent = new HashMap<>();
-        
+
         parseEmail();
     }
 
     private void parseEmail() throws Exception {
         java.util.Properties properties = System.getProperties();
         Session session = Session.getDefaultInstance(properties);
-        
+
         FileInputStream fis = null;
         try {
             fis = new FileInputStream(emailFile);
@@ -85,19 +86,19 @@ public class EmlParser implements EmailDataProvider {
             _to = email.getRecipients(RecipientType.TO);
             _from = email.getFrom();
             _subject = email.getSubject();
-            _content =  email.getContent();
+            _content = email.getContent();
             _date = email.getReceivedDate();
             _sentDate = email.getSentDate();
             //System.out.println("content type: " + email.getContentType());
             //System.out.println("\nsubject: " + email.getSubject());
             //to = EmailUtil.parseAddressLines(email
-             //       .getHeader(Message.RecipientType.TO.toString()));
+            //       .getHeader(Message.RecipientType.TO.toString()));
         } catch (MessagingException e) {
             throw new IllegalStateException("illegal state issue", e);
         } catch (FileNotFoundException e) {
             throw new IllegalStateException("file not found issue issue: "
                     + emailFile.getAbsolutePath(), e);
-        } catch (Exception e) {
+        } catch (IOException e) {
             log.error("Problem parsing eml file", e);
         } finally {
             if (fis != null) {
@@ -109,75 +110,75 @@ public class EmlParser implements EmailDataProvider {
             }
         }
     }
-    
+
     private List<String> getAddressAsList(Address[] address) {
-        List<String> result = new ArrayList<String>();
-        if(address!=null) {
-            for(Address a : address) {
+        List<String> result = new ArrayList<>();
+        if (address != null) {
+            for (Address a : address) {
                 result.add(a.toString());
             }
         }
         return result;
     }
-    
+
     @Override
     public List<String> getFrom() {
         return getAddressAsList(_from);
     }
-    
+
     @Override
     public List<String> getRecepient() {
         return getAddressAsList(_to);
     }
-    
+
     @Override
     public List<String> getCC() {
         return getAddressAsList(_cc);
     }
-    
+
     @Override
     public List<String> getBCC() {
         return getAddressAsList(_bcc);
     }
-    
+
     @Override
     public String getSubject() {
         return _subject;
     }
-    
+
     @Override
     public Date getDate() {
         return _date;
     }
-    
+
     @Override
     public String getContent() throws MessagingException, IOException {
         if (_content instanceof String) {
             return _content.toString();
-        } 
-        
+        }
+
         if (_content instanceof MimeMultipart) {
-            MimeMultipart cnt = (MimeMultipart)_content;
+            MimeMultipart cnt = (MimeMultipart) _content;
             int size = cnt.getCount();
             StringBuilder res = new StringBuilder();
-            
+
             for (int i = 0; i < size; ++i) {
                 BodyPart bp = cnt.getBodyPart(i);
                 res.append(dumpPart(bp));
-            } 
+            }
             return res.toString();
         }
-        
+
         return "";
     }
-    
+
     private String dumpPart(Part p) throws MessagingException, IOException {
-        
+
         StringBuilder buf = new StringBuilder();
-        if(p.isMimeType("text/plain")) {
+        if (p.isMimeType("text/plain")) {
             buf.append(p.getContent());
-        } else if(p.isMimeType("multipart/*")) {
-            Multipart mp = (Multipart)p.getContent();
+        } else if (p.isMimeType("multipart/*")) {
+            Multipart mp = (Multipart) p.getContent();
             int count = mp.getCount();
             for (int i = 0; i < count; i++) {
                 BodyPart bp = mp.getBodyPart(i);
@@ -186,36 +187,35 @@ public class EmlParser implements EmailDataProvider {
                 }
             }
         } else if (p.isMimeType("message/rfc822")) {
-            buf.append(dumpPart((Part)p.getContent()));
-        } else
-            {
+            buf.append(dumpPart((Part) p.getContent()));
+        } else {
             String disp = null;
             try {
-                disp = p.getDisposition();                
+                disp = p.getDisposition();
             } catch (Exception e) {
             }
-            
+
             String filename = "attach-" + (attachmentSeq++);
             try {
                 filename = p.getFileName();
             } catch (Exception e) {
                 log.error("Problem getting the real attachment name", e);
             }
-            
+
             if (disp == null || disp.equalsIgnoreCase(Part.ATTACHMENT)) {
                 log.debug("Adding attachment: " + filename);
-                
+
                 _attachments.add(filename);
-                
+
                 if (Project.getProject().isAddEmailAttachmentToPDF()) {
                     log.debug("Parsing the attachment content with Tika");
-                    
+
                     Tika tika = new Tika();
                     tika.setMaxStringLength(10 * 1024 * 1024);
                     try {
                         String attachmentContent = tika.parseToString(p.getInputStream(), new Metadata());
                         attachmentsContent.put(filename, attachmentContent);
-                        
+
                         log.debug("Attachment content parsed!");
                     } catch (TikaException e) {
                         log.error("Problem parsing attachment", e);
@@ -247,16 +247,14 @@ public class EmlParser implements EmailDataProvider {
     }
 
     /**
-     * @param emailFile
-     *            the emailFile to set
+     * @param emailFile the emailFile to set
      */
     public void setEmailFile(File emailFile) {
         this.emailFile = emailFile;
     }
 
     /**
-     * @param to
-     *            the to to set
+     * @param to the to to set
      */
     public void setTo(ArrayList<String> to) {
         this.to = to;
@@ -265,18 +263,21 @@ public class EmlParser implements EmailDataProvider {
     /**
      * @return the to
      */
+    @Override
     public List<String> getTo() {
         return getAddressAsList(_to);
     }
-    
+
+    @Override
     public List<String> getAttachmentNames() {
         return _attachments;
     }
 
+    @Override
     public Date getSentDate() {
         return this._sentDate;
     }
-    
+
     public void saveAttachments() throws MessagingException, IOException {
         if (email.isMimeType("text/*")) {
             // no attachments there - this is just the email itself
@@ -302,7 +303,8 @@ public class EmlParser implements EmailDataProvider {
             }
         }
     }
-    
+
+    @Override
     public Map<String, String> getAttachmentsContent() {
         return attachmentsContent;
     }
