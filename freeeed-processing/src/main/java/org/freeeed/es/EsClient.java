7cf00726b56edc33986104e335a5b4b5ea451de6
Mark Kerzner <mark@elephantscale.com>
2016-07-01 01:08:42 -0500

    elastic search work

---
+++ freeeed-processing/src/main/java/org/freeeed/es/EsClient.java
@@ -0,0 +1,60 @@
+/*
+ *
+ * Copyright SHMsoft, Inc. 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * ElasticSearch operations
+ */
+package org.freeeed.es;
+
+import java.net.InetAddress;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+
+/**
+ *
+ * @author mark
+ */
+public class EsClient {
+
+    private String host;
+
+    private Client client;
+
+    public void open() throws Exception {
+        client = TransportClient.builder().build()
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), 9200));
+
+    }
+
+    public void close() {
+        client.close();
+    }
+
+    /**
+     * @return the host
+     */
+    public String getHost() {
+        return host;
+    }
+
+    /**
+     * @param host the host to set
+     */
+    public void setHost(String host) {
+        this.host = host;
+    }
+}
---
+++ freeeed-processing/src/test/java/org/freeeed/es/EsClientTest.java
@@ -0,0 +1,63 @@
+/*
+ *
+ * Copyright SHMsoft, Inc. 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.freeeed.es;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mark
+ */
+public class EsClientTest {
+    
+    public EsClientTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of open method, of class EsClient.
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testOpen() throws Exception {
+        System.out.println("open");
+        String host = "http://cd563a0b2c895eba8facac558b3e887a.us-east-1.aws.found.io";
+        EsClient instance = new EsClient();
+        instance.setHost(host);
+        instance.open();
+    }    
+}
