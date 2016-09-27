f415c52f87670e57a64620ba1bc2e847ae0ae993
Mark Kerzner <mark@elephantscale.com>
2016-09-26 23:24:16 -0500

    doing better stop_all.sh

--- stop_all.sh
+++ stop_all.sh
@@ -8,9 +8,11 @@ cd freeeed-tomcat/bin;
 cd ../..
 
 cd freeeed-solr/example
-java -Xmx1024M -jar start.jar &
+# java -Xmx1024M -jar start.jar &
+# TODO - kill the process by id?
 
 cd ../..
 
 cd FreeEed
-./freeeed_player.sh &
+#./freeeed_player.sh &
+# TODO - kill the process by id?
