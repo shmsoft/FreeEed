d29249d9d398b656d31255dcd0ba449716b83adb
Mark Kerzner <mark@elephantscale.com>
2017-05-11 23:44:05 -0500

    fixes fixes

--- freeeed-test/src/test/java/org/freeeed/ocr/OCRProcessorTest.java
+++ freeeed-test/src/test/java/org/freeeed/ocr/OCRProcessorTest.java
@@ -20,7 +20,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 // needed for @Test
-import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
