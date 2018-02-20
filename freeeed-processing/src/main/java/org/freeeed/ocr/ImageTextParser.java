96f882277d76a522f445c7b2c45f99cbfc6935e1
Neha Ojha <nehadubey0605@gmail.com>
2018-02-20 09:17:04 +0530

    parallel processing for ocr files

--- freeeed-processing/src/main/java/org/freeeed/ocr/ImageTextParser.java
+++ freeeed-processing/src/main/java/org/freeeed/ocr/ImageTextParser.java
@@ -16,6 +16,8 @@
  */
 package org.freeeed.ocr;
 
+import org.apache.pdfbox.multipdf.Splitter;
+import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.tika.Tika;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
@@ -30,7 +32,12 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 /**
  * This class parses file with or without embedded documents
@@ -45,6 +52,9 @@ public class ImageTextParser {
     private static final ParseContext PARSE_CONTEXT = new ParseContext();
     private static final String EMPTY = "";
     private static final Project CURRENT_PROJECT = Project.getCurrentProject();
+    private static final String TMP = "/tmp/";
+    private static final AtomicInteger COUNTER = new AtomicInteger();
+
 
     //config
     static {
@@ -65,17 +75,6 @@ public class ImageTextParser {
         return parseImages(file);
     }
 
-    private static String parseImages(String file) {
-        try (InputStream stream = new FileInputStream(file)) {
-            BodyContentHandler handler = new BodyContentHandler(Integer.MAX_VALUE);
-            AUTO_DETECT_PARSER.parse(stream, handler, new Metadata(), PARSE_CONTEXT);
-            return handler.toString().trim();
-        } catch (Exception ex) {
-            LOGGER.error("Problem parsing document {}", file, ex);
-        }
-        return EMPTY;
-    }
-
     private static String parseText(String filePath) {
         try {
             return TIKA.parseToString(new File(filePath));
@@ -84,4 +83,76 @@ public class ImageTextParser {
         }
         return EMPTY;
     }
+
+    private static String parseImages(String file) {
+        //break file into pages, do ocr and then send parsed contents
+        try {
+            String pagePath = splitPages(file);
+            return ocrParallel(pagePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return EMPTY;
+    }
+
+    private static String ocrParallel(String pagePath) {
+        File root = new File(pagePath);
+        File[] files = root.listFiles();
+        String[] contents = new String[files.length];
+        COUNTER.set(0);
+        IntStream.rangeClosed(1, contents.length).parallel().forEach(i -> {
+            File file = new File(pagePath + i + ".pdf");
+            parseAndPut(contents, i, file);
+        });
+        return combineAll(contents);
+    }
+
+    private static void parseAndPut(String[] contents, int i, File file) {
+        try (InputStream stream = new FileInputStream(file)) {
+            BodyContentHandler handler = new BodyContentHandler(Integer.MAX_VALUE);
+            AUTO_DETECT_PARSER.parse(stream, handler, new Metadata(), PARSE_CONTEXT);
+            contents[i] = handler.toString().trim();
+            file.delete();
+            int count = COUNTER.incrementAndGet();
+            System.out.println("progress " + count + " of " + contents.length);
+        } catch (Exception ex) {
+            LOGGER.error("Problem parsing document {}", file, ex);
+        }
+    }
+
+    private static String combineAll(String[] contents) {
+        StringBuilder content = new StringBuilder();
+        for (String str : contents) {
+            content.append(str);
+        }
+        return content.toString();
+    }
+
+    private static String splitPages(String filePath) throws IOException {
+        File file = new File(filePath);
+        String pagePath;
+        try (PDDocument document = PDDocument.load(file)) {
+            Splitter splitter = new Splitter();
+            List<PDDocument> pages = splitter.split(document);
+            Iterator<PDDocument> iterator = pages.listIterator();
+            int i = 1;
+            pagePath = createTempPath(file);
+            System.out.println("pagePath = " + pagePath);
+            while (iterator.hasNext()) {
+                PDDocument pd = iterator.next();
+                pd.save(pagePath + i++ + ".pdf");
+            }
+        }
+        return pagePath;
+    }
+
+    private static String createTempPath(File file) {
+        String pagePath = TMP + file.getName() + "/";
+        File tempFile = new File(pagePath);
+        if (!tempFile.exists()) {
+            tempFile.mkdirs();
+        }
+        return pagePath;
+    }
+
 }
