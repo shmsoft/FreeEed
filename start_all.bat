d1e38868ed69d8baba8c23815be49015c50def03
Mark Kerzner <mark@elephantscale.com>
2018-07-22 13:19:18 -0500

    debugging

--- start_all.bat
+++ start_all.bat
@@ -1,14 +1,18 @@
 echo off
 
 cd freeeed-tomcat\bin
+
 startup.bat
+cd ..\..
+
+
+cd elasticsearch-6.2.2\bin
+start elasticsearch.bat
+
 
 cd ..\..
 
-cd elasticsearch-6.2.2
-bin/elasticsearch.bat
-
-cd ..\..
 
 cd FreeEed
+
 start freeeed_player.bat
