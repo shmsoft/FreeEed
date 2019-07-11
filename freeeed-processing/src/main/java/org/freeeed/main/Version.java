3945d6ca2c62e262cd2a1cd4ee052467cf864edb
Mark Kerzner <mark@elephantscale.com>
2019-07-10 23:33:36 -0500

    start work on v8

--- freeeed-processing/src/main/java/org/freeeed/main/Version.java
+++ freeeed-processing/src/main/java/org/freeeed/main/Version.java
@@ -25,7 +25,7 @@ import java.util.Date;
  * @author mark
  */
 public class Version {
-    private static final String V = "7.7.5";
+    private static final String V = "8.0.0";
     public static String getVersionAndBuild() {
         return ParameterProcessing.APP_NAME + " " + getVersionNumber()                
                 + ", build date: " + getBuildTime();
