5219c86b963aee0ff93c10d96e34e36798d3d2e5
farshidmh <mehrtash.fm@live.com>
2020-02-11 17:00:32 +0330

    Clean up reporting extracted file size

--- freeeed-processing/src/main/java/org/freeeed/extractor/ZipFileExtractor.java
+++ freeeed-processing/src/main/java/org/freeeed/extractor/ZipFileExtractor.java
@@ -4,6 +4,7 @@ import net.lingala.zip4j.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 import net.lingala.zip4j.model.FileHeader;
 import org.freeeed.mr.FreeEedMR;
+import org.freeeed.services.ProcessingStats;
 import org.freeeed.services.Project;
 import org.freeeed.services.UniqueIdGenerator;
 
@@ -13,26 +14,22 @@ import java.util.List;
 public class ZipFileExtractor implements Runnable {
 
     private File file;
-    String tmpFolder;
-    Project project;
-    boolean parent = false;
-    String FileId = UniqueIdGenerator.INSTANCE.getNextZIPFolderId();
+    private String tmpFolder;
 
     public ZipFileExtractor(Project project, File file) {
-        this.project = project;
         this.file = file;
-        tmpFolder = project.getStagingDir() + "\\" + FileId + "_" + file.getName() + "\\";
+        String fileId = UniqueIdGenerator.INSTANCE.getNextZIPFolderId();
+        tmpFolder = project.getStagingDir() + "\\" + fileId + "_" + file.getName() + "\\";
         new File(tmpFolder).mkdirs();
     }
-
 
     @Override
     public void run() {
         ZipFile zipFile = new ZipFile(file);
         try {
-            List fileHeaderList = zipFile.getFileHeaders();
-            for (int i = 0; i < fileHeaderList.size(); i++) {
-                FileHeader fileHeader = (FileHeader) fileHeaderList.get(i);
+            List<FileHeader> fileHeaderList = zipFile.getFileHeaders();
+            for (Object o : fileHeaderList) {
+                FileHeader fileHeader = (FileHeader) o;
                 String newFileName;
                 if (fileHeader.getFileName().endsWith(".zip")) {
                     newFileName = fileHeader.getFileName();
@@ -41,12 +38,13 @@ public class ZipFileExtractor implements Runnable {
                     newFileName = FileId + "_" + fileHeader.getFileName();
                 }
                 zipFile.extractFile(fileHeader, tmpFolder, newFileName);
+                ProcessingStats.getInstance().addzipFilExtractedSize(fileHeader.getUncompressedSize());
             }
         } catch (ZipException e) {
             e.printStackTrace();
         }
 
         file.delete();
-        FreeEedMR.reduceZipFile();
+        FreeEedMR.getInstance().reduceZipFile();
     }
 }
