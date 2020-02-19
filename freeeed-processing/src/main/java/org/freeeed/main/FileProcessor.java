1ca2422a62ee1eb29877f1b6c2ff7ece8ed802b5
farshidmh <mehrtash.fm@live.com>
2020-02-20 00:52:30 +0330

    V8

--- freeeed-processing/src/main/java/org/freeeed/main/FileProcessor.java
+++ freeeed-processing/src/main/java/org/freeeed/main/FileProcessor.java
@@ -20,7 +20,6 @@ import com.google.common.io.Files;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
 import org.apache.hadoop.io.BytesWritable;
-import org.apache.hadoop.io.MD5Hash;
 import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.lucene.queryparser.classic.ParseException;
@@ -34,7 +33,6 @@ import org.freeeed.util.Util;
 import org.jsoup.Jsoup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -49,29 +47,39 @@ public class FileProcessor implements Runnable {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(FileProcessor.class);
     protected String zipFileName;
-    protected String singleFileName;
-    protected MetadataWriter metadataWriter;
+    private String singleFileName;
     DiscoveryFile discoveryFile;
+    DocumentMetadata metadata;
 
-
-    public FileProcessor() {
-    }
-
-
-    public String getZipFileName() {
-        return zipFileName;
+    FileProcessor() {
     }
 
     public String getSingleFileName() {
         return singleFileName;
     }
 
-    public FileProcessor(MetadataWriter metadataWriter,  DiscoveryFile discoveryFile) {
-        this.metadataWriter = metadataWriter;
+    public FileProcessor(DiscoveryFile discoveryFile) {
         this.discoveryFile = discoveryFile;
+        initMetaData();
     }
 
-    protected boolean isPreview() {
+    void initMetaData() {
+        metadata = new DocumentMetadata();
+        discoveryFile.setMetadata(metadata);
+        metadata.setOriginalPath(getOriginalDocumentPath(discoveryFile));
+        metadata.setHasAttachments(discoveryFile.isHasAttachments());
+        metadata.setHasParent(discoveryFile.isHasParent());
+        String hash = null;
+        try {
+            hash = Util.createKeyHash(discoveryFile.getPath(), metadata);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        metadata.setHash(hash);
+        metadata.acquireUniqueId();
+    }
+
+    boolean isPreview() {
         return Project.getCurrentProject().isPreview();
     }
 
@@ -79,13 +87,13 @@ public class FileProcessor implements Runnable {
         return Project.getCurrentProject().isCreatePDF();
     }
 
-    protected void createImage(DiscoveryFile discoveryFile) {
+    void createImage(DiscoveryFile discoveryFile) {
         if (isPdf()) {
             OfficePrint.getInstance().createPdf(discoveryFile.getPath(), discoveryFile.getRealFileName());
         }
     }
 
-    protected void createHtmlForDocument(DiscoveryFile discoveryFile) throws Exception {
+    void createHtmlForDocument(DiscoveryFile discoveryFile) throws Exception {
         //first make sure the output directory is empty
         File outputDir = new File(getHtmlOutputDir());
         if (outputDir.exists()) {
@@ -116,13 +124,15 @@ public class FileProcessor implements Runnable {
     }
 
     void writeMetadata() {
+        /*
         String fileName = discoveryFile.getPath().getPath();
-        MapWritable mapWritable = new MapWritable();
+        HashMap<String, String> mapWritable = new HashMap();
         String[] names = discoveryFile.getMetadata().names();
         for (String name : names) {
-            System.out.println(name);
-            mapWritable.put(new Text(name), new Text(discoveryFile.getMetadata().get(name)));
+            mapWritable.put(name, discoveryFile.getMetadata().get(name));
         }
+        mapWritable.put( DocumentMetadataKeys.STAGE_PATH , fileName.toString());
+
         byte[] bytes = new byte[0];
         try {
             bytes = Util.getFileContent(fileName);
@@ -130,6 +140,7 @@ public class FileProcessor implements Runnable {
             e.printStackTrace();
         }
         mapWritable.put(new Text(ParameterProcessing.NATIVE), new BytesWritable(bytes));
+
         if (isPdf()) {
             String pdfFileName = fileName + ".pdf";
             if (new File(pdfFileName).exists()) {
@@ -147,41 +158,17 @@ public class FileProcessor implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        discoveryFile.setMetaWriteable(mapWritable);
+*/
+
+
         try {
-            MetadataWriter.getInstance().processMap(mapWritable, discoveryFile);
+            MetadataWriter.getInstance().processMap(discoveryFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
 
-    /**
-     * Create a map
-     *
-     * @param metadata Hadoop metadata to insert into map
-     * @return Created map
-     * @throws IOException
-     */
-    private MapWritable createMapWritable(Metadata metadata, DiscoveryFile discoveryFile) throws IOException {
-        String fileName = discoveryFile.getPath().getPath();
-        MapWritable mapWritable = new MapWritable();
-        String[] names = metadata.names();
-        for (String name : names) {
-            mapWritable.put(new Text(name), new Text(metadata.get(name)));
-        }
-        byte[] bytes = Util.getFileContent(fileName);
-        mapWritable.put(new Text(ParameterProcessing.NATIVE), new BytesWritable(bytes));
-
-        if (isPdf()) {
-            String pdfFileName = fileName + ".pdf";
-            if (new File(pdfFileName).exists()) {
-                byte[] pdfBytes = Util.getFileContent(pdfFileName);
-                mapWritable.put(new Text(ParameterProcessing.NATIVE_AS_PDF), new BytesWritable(pdfBytes));
-            }
-        }
-
-        createMapWritableForHtml(mapWritable);
-
-        return mapWritable;
     }
 
     /**
@@ -230,7 +217,7 @@ public class FileProcessor implements Runnable {
      * @param metadata
      * @return true if match is found else false
      */
-    protected boolean isResponsive(Metadata metadata) throws IOException, ParseException {
+    boolean isResponsive(Metadata metadata) throws IOException, ParseException {
         // set true if search finds a match
         /*boolean isResponsive;
         // get culling parameters
@@ -272,7 +259,7 @@ public class FileProcessor implements Runnable {
         return true;
     }
 
-    protected void addToES(Metadata metadata) {
+    void addToES(Metadata metadata) {
         ESIndex.getInstance().addBatchData(metadata);
     }
 
@@ -282,15 +269,15 @@ public class FileProcessor implements Runnable {
      *
      * @return DocumentMetadata container receiving metadata.
      */
-    protected void extractMetadata(DiscoveryFile discoveryFile, DocumentMetadata metadata) {
-        DocumentParser.getInstance().parse(discoveryFile, metadata);
+    void extractMetadata() {
+        DocumentParser.getInstance().parse(discoveryFile);
     }
 
     public String getOriginalDocumentPath(DiscoveryFile discoveryFile) {
-        return discoveryFile.getRealFileName();
+        return discoveryFile.getPath().toString();
     }
 
-    protected void extractJlFields(DiscoveryFile discoveryFile) {
+    void extractJlFields(DiscoveryFile discoveryFile) {
         LineIterator it = null;
         try {
             it = FileUtils.lineIterator(discoveryFile.getPath(), "UTF-8");
@@ -307,7 +294,7 @@ public class FileProcessor implements Runnable {
                 metadata.setHasAttachments(discoveryFile.isHasAttachments());
                 metadata.setHasParent(discoveryFile.isHasParent());
                 metadata.setCustodian(Project.getCurrentProject().getCurrentCustodian());
-                writeMetadata(discoveryFile, metadata);
+                writeMetadata();
                 ESIndex.getInstance().addBatchData(metadata);
             }
         } catch (Exception e) {
@@ -328,43 +315,19 @@ public class FileProcessor implements Runnable {
         // exception message to place in output if error occurs
         String exceptionMessage = null;
         // ImageTextParser metadata, derived from Tika metadata class
-        DocumentMetadata metadata = new DocumentMetadata();
-        discoveryFile.setMetadata(metadata);
         String extension = Util.getExtension(discoveryFile.getRealFileName());
-
         if ("jl".equalsIgnoreCase(extension)) {
             extractJlFields(discoveryFile);
         }
         try {
-            metadata.setOriginalPath(getOriginalDocumentPath(discoveryFile));
-            metadata.setHasAttachments(discoveryFile.isHasAttachments());
-            metadata.setHasParent(discoveryFile.isHasParent());
-            // extract file contents with Tika
-            // Tika metadata class contains references to metadata and file text
-            // TODO discoveryFile has the pointer to the same metadata - simplify this
-            extractMetadata(discoveryFile, metadata);
-
-            /*
-            if (project.isRemoveSystemFiles() && Util.isSystemFile(metadata)) {
-                LOGGER.info("File {} is recognized as system file and is not processed further",
-                        discoveryFile.getPath().getPath());
-                return;
-            }
-            metadata.setCustodian(project.getCurrentCustodian());
-            */
-            // add Hash to metadata
-
-            MD5Hash hash = Util.createKeyHash(discoveryFile.getPath(), metadata);
-            metadata.setHash(hash.toString());
-            metadata.acquireUniqueId();
+            extractMetadata();
+            //metadata.setCustodian(project.getCurrentCustodian());
             // search through Tika results using Lucene
             isResponsive = isResponsive(metadata);
             if (isResponsive) {
                 addToES(metadata);
             }
-
         } catch (IOException | ParseException e) {
-            // LOGGER.warn("Exception processing file ", e);
             exceptionMessage = e.getMessage();
             metadata.set(DocumentMetadataKeys.PROCESSING_EXCEPTION, exceptionMessage);
         }
@@ -378,15 +341,8 @@ public class FileProcessor implements Runnable {
                     metadata.set(DocumentMetadataKeys.PROCESSING_EXCEPTION, e.getMessage());
                 }
             }
-            try {
-                writeMetadata(discoveryFile, metadata);
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
+
+            writeMetadata();
         }
-        //Stats.getInstance().increaseItemCount(discoveryFile.getPath().length());
-
-
-        //LOGGER.trace("Is the file responsive: {}", isResponsive);
     }
 }
