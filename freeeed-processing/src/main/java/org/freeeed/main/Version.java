508151e3555c0670c32a7c2f7cd05339a548ad0a
farshidmh <mehrtash.fm@live.com>
2020-03-04 22:44:27 +0330

    clean up

--- freeeed-processing/src/main/java/org/freeeed/main/Version.java
+++ freeeed-processing/src/main/java/org/freeeed/main/Version.java
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright SHMsoft, Inc. 
+ * Copyright SHMsoft, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,15 +13,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 package org.freeeed.main;
 
-import java.io.File;
-import java.util.Date;
-
-
 /**
- *
  * @author mark
  */
 public class Version {
@@ -31,21 +26,18 @@ public class Version {
                 + ", build date: " + getBuildTime();
     }       
 
-    private static String getBuildTime() {
-        String buildTime = "Unknown";
-        String jarFileName = "target/freeeed-processing-1.0-SNAPSHOT-jar-with-dependencies.jar";
-        File file = new File(jarFileName);
-        if (file.exists()) {
-            Date lastModified = new Date(file.lastModified());
-            buildTime = lastModified.toString();
-        }
-        return buildTime;
+    static String getVersionAndBuild() {
+        return ParameterProcessing.APP_NAME + " " + getVersionNumber() + " Build " + getBuildNumber();
     }
-    
+
+    private static String getBuildNumber() {
+        return BuildNumber;
+    }
+
     public static String getVersion() {
         return ParameterProcessing.APP_NAME + " " + getVersionNumber();
     }
-    
+
     private static String getVersionNumber() {
         return V;
     }
