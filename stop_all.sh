7d8f84488bcfca812853e4ffd2acc1425cbafea0
Mark Kerzner <mark@elephantscale.com>
2020-08-14 00:56:54 -0500

    8.0.5 version

--- stop_all.sh
+++ stop_all.sh
@@ -9,10 +9,8 @@ cd ../..
 
 cd freeeed-solr/example
 # java -Xmx1024M -jar start.jar &
-# TODO - kill the process by id?
 
 cd ../..
 
 cd FreeEed
 #./freeeed_player.sh &
-# TODO - kill the process by id?
