0efe69ee20425b8dd4e25b40a4650a18e55402be
Mark Kerzner <mark@elephantscale.com>
2021-12-14 23:18:40 -0600

    Fixed #284

--- freeeed-processing/src/test/java/org/freeeed/ai/inabia/PiiTest.java
+++ freeeed-processing/src/test/java/org/freeeed/ai/inabia/PiiTest.java
@@ -9,8 +9,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  *
@@ -22,18 +21,16 @@ public class PiiTest {
         System.out.println("testGetPii");
         String str = "Hello 713-777-7777 Name: John Doe, johndoe@gmail.com. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 1301 McKinney St #2400, Houston, TX 77010";
 
-
-        String str2 = "This is how I tried to split a paragraph into a sentence. But, there is a problem. My paragraph includes dates like Jan.13, 2014 , words like U.S and numbers like 2.2. They all got split by the above code. we might have a credit card number like: 6219-8610-2502-0511 or a cc number like: 6219861025020511. another possibility is a word with SSN in it likeL:  859-98-0987 or like 859980987. now, another type is CC number is possible: 6219 8610 2502 0511";
-
-        InabiaClient a = new InabiaClient(str,"SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",100);
+        InabiaClient client = new InabiaClient(str,"SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",100);
 
         try {
-            System.out.println(a.getPII());
+            String expectedResults = "{Address=1301 Mckinney St #2400, Houston, Tx 77010, Phone=713-777-7777, Name=John Doe}";
+            String returnResult = client.getPII().toString();
+            assertEquals(expectedResults, returnResult);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        assertTrue(true);
     }
 }
