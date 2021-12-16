68c5b71e823ae59abd8e694909fdd72f8cff88dc
Mark Kerzner <mark@elephantscale.com>
2021-12-16 00:34:33 -0600

    Fixed #284

--- freeeed-processing/src/test/java/org/freeeed/ai/inabia/PiiTest.java
+++ freeeed-processing/src/test/java/org/freeeed/ai/inabia/PiiTest.java
@@ -20,8 +20,8 @@ public class PiiTest {
     public void testGetPii() {
         System.out.println("testGetPii");
         String str = "Hello 713-777-7777 Name: John Doe, johndoe@gmail.com. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 1301 McKinney St #2400, Houston, TX 77010";
-
-        InabiaClient client = new InabiaClient(str,"SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",100);
+        String inabia_token = System.getenv("INABIA_TOKEN");
+        InabiaClient client = new InabiaClient(str,inabia_token,100);
 
         try {
             String expectedResults = "{Address=1301 Mckinney St #2400, Houston, Tx 77010, Phone=713-777-7777, Name=John Doe}";
