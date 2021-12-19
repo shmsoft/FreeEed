7f7901aaf5d179cbfb812f7546c82b032050f994
Mark Kerzner <mark@elephantscale.com>
2021-12-19 10:34:08 -0600

    Closed #283

--- release_freeeed_complete.sh
+++ release_freeeed_complete.sh
@@ -131,19 +131,19 @@ if [ "$UPLOAD_TO_S3_FREEEED_PLAYER" == true ]; then
     echo "Uploading $VERSION/FreeEed-$VERSION.zip to s3://shmsoft/releases/"
     echo "CURR_DIR=" $CURR_DIR
     cd $CURR_DIR || exit
-    s3cmd -P put FreeEed-$VERSION.zip s3://shmsoft/releases/
+    aws s3 cp FreeEed-$VERSION.zip s3://shmsoft/releases/
 fi
 
 if [ "$UPLOAD_TO_S3_FREEEED_UI" == true ]; then
     echo "Uploading to S3.... freeeedui-$VERSION.war"
     cd $CURR_DIR || exit
-    s3cmd -P put freeeedui-$VERSION.war s3://shmsoft/releases/
+    aws s3 cp freeeedui-$VERSION.war s3://shmsoft/releases/
 fi
 
 if [ "$UPLOAD_TO_S3_FREEEED_PACK" == true ]; then
     echo "Uploading to S3.... freeeed_complete_pack-$VERSION.zip"
     cd $CURR_DIR || exit
-    s3cmd -P put freeeed_complete_pack-$VERSION.zip s3://shmsoft/releases/
+    aws s3 cp freeeed_complete_pack-$VERSION.zip s3://shmsoft/releases/
 fi
 
 
