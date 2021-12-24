7f1a405299ab88ae2b47da3dffc7f3f277e5a7d6
Mark Kerzner <mark@elephantscale.com>
2021-12-24 15:21:30 -0600

    PII works with parsing, waiting for one small reqest: take out 'confidence'

--- freeeed-processing/src/test/java/org/freeeed/ai/inabia/PiiTest.java
+++ freeeed-processing/src/test/java/org/freeeed/ai/inabia/PiiTest.java
@@ -4,9 +4,13 @@
  */
 package org.freeeed.ai.inabia;
 
+import com.google.gson.JsonObject;
 import okhttp3.*;
-import org.freeeed.services.Project;
 import org.junit.Test;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
 
 import java.io.IOException;
 
@@ -17,34 +21,14 @@ import static org.junit.Assert.*;
  * @author mark
  */
 public class PiiTest {
-    //@Test
-    public void testGetPii() {
-        System.out.println("testGetPii");
-        String str = "Hello 713-777-7777 Name: John Doe, johndoe@gmail.com. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 1301 McKinney St #2400, Houston, TX 77010";
-        String inabia_token = System.getenv("INABIA_TOKEN");
-        InabiaClient client = new InabiaClient(str,inabia_token,100);
-
-        try {
-            String expectedResults = "{Address=1301 Mckinney St #2400, Houston, Tx 77010, Phone=713-777-7777, Name=John Doe}";
-            String returnResult = client.getPII().toString();
-            assertEquals(expectedResults, returnResult);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
     @Test
     public void testInabiaPii() {
         System.out.println("testInabiaPii");
         String data = "Hello 713-777-7777 Name: John Doe, johndoe@gmail.com. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 1301 McKinney St #2400, Houston, TX 77010";
-        //String API_URL = "https://inabia.ai:8000/extractPII";
-        String API_URL = "https://vp3xir2ce6.execute-api.us-west-2.amazonaws.com/extractPII";
-
+        String API_URL = "https://inabia.ai:8000/extractPII";
+        //String API_URL = "https://vp3xir2ce6.execute-api.us-west-2.amazonaws.com/extractPII";
         data = data.replaceAll("<br>", " ").trim();
         data = "{ \"text\":" + "\"" + data + "\"}";
-
-
         try {
             OkHttpClient client = new OkHttpClient().newBuilder()
                     .build();
@@ -59,13 +43,23 @@ public class PiiTest {
                     .addHeader("Content-Type", "application/json")
                     .build();
             Response response = client.newCall(request).execute();
-            String jsonData = response.body().string();
-
-            System.out.println(jsonData);
-
+            String jsonString = response.body().string();
+            System.out.println(jsonString);
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(jsonString);
+            System.out.println(obj);
+            JSONObject jsonObject = (JSONObject) obj;
+            System.out.println(jsonObject);
+            JSONArray jsonResponse = (JSONArray) jsonObject.get("response");
+            System.out.println(jsonResponse);
+            JSONArray pii = (JSONArray) jsonResponse.get(1);
+            System.out.println(pii);
+            for (int i = 0; i < pii.size(); ++i) {
+                JSONObject piiElement = (JSONObject) pii.get(i);
+                System.out.println(piiElement);
+            }
         } catch (Exception e) {
             System.out.println("Exception in NetClientGet:- " + e);
-
         }
     }
 }
