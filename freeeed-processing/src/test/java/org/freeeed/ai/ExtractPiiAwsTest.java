bed0722c077c353ff1d0760d22203c0fb0009d4c
Mark Kerzner <mark@elephantscale.com>
2021-12-28 15:54:04 -0600

    Closing #302

--- freeeed-processing/src/test/java/org/freeeed/ai/ExtractPiiAwsTest.java
+++ freeeed-processing/src/test/java/org/freeeed/ai/ExtractPiiAwsTest.java
@@ -4,32 +4,25 @@
  */
 package org.freeeed.ai;
 
-import okhttp3.*;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- *
  * @author mark
  * For testing
-aws comprehend detect-pii-entities \
---text "Hello 713-777-7777 Name: John Doe, johndoe@gmail.com. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 1301 McKinney St #2400, Houston, TX 77010" \
---language-code en
+ * aws comprehend detect-pii-entities \
+ * --text "Hello 713-777-7777 Name: John Doe, johndoe@gmail.com. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 1301 McKinney St #2400, Houston, TX 77010" \
+ * --language-code en
  */
 public class ExtractPiiAwsTest {
     @Test
     public void testInabiaPii() {
         System.out.println("ExtractPiiAwsTest");
-        String response = new ExtractPiiAws().extractPiiAsString("It does not matter");
+        String data = "Hello 713-777-7777 Name: John Doe, johndoe@gmail.com. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 1301 McKinney St #2400, Houston, TX 77010";
+        String response = new ExtractPiiAws().extractPiiAsString(data);
         System.out.println(response);
-        assertTrue(response.length() > 0);
+        // 24 characters is empty response
+        assertTrue(response.length() > 24);
     }
 }
