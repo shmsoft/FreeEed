0cec683fc1a164ac01a2d2982ae86af04012e1ae
Mark Kerzner <mark@elephantscale.com>
2021-12-29 16:14:08 -0600

    Closing #302

--- freeeed-processing/src/test/java/org/freeeed/ai/ExtractPiiAwsTest.java
+++ freeeed-processing/src/test/java/org/freeeed/ai/ExtractPiiAwsTest.java
@@ -4,7 +4,11 @@
  */
 package org.freeeed.ai;
 
+import org.junit.Before;
 import org.junit.Test;
+import software.amazon.awssdk.regions.Region;
+
+import java.util.HashMap;
 
 import static org.junit.Assert.assertTrue;
 
@@ -16,13 +20,27 @@ import static org.junit.Assert.assertTrue;
  * --language-code en
  */
 public class ExtractPiiAwsTest {
+    private String data = "Hello 713-777-7777 Name: John Doe, johndoe@gmail.com. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 1301 McKinney St #2400, Houston, TX 77010";
+
+    private String awsAccessKeyId = "";
+    private String awsSecretAccessKey = "";
+    private Region awsRegion = Region.US_EAST_1;
+
+    @Before
+
+    public void initKeys() {
+        awsAccessKeyId = System.getenv("aws_access_key_id");
+        awsSecretAccessKey = System.getenv("aws_secret_access_key");
+    }
+
     @Test
     public void testAwsPii() {
+
+
         System.out.println("ExtractPiiAwsTest");
         String data = "Hello 713-777-7777 Name: John Doe, johndoe@gmail.com. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 1301 McKinney St #2400, Houston, TX 77010";
-        String response = new ExtractPiiAwsCLI().extractPiiAsString(data);
-        System.out.println(response);
-        // 24 characters is empty response
-        assertTrue(response.length() > 24);
+        ExtractPiiAws extractor = new ExtractPiiAws(awsAccessKeyId, awsSecretAccessKey, awsRegion);
+        HashMap pii = extractor.extractPII(data);
+        assertTrue(pii.size() > 0);
     }
 }
