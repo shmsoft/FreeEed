3d00a11335fe14a8f929cd732c3cf75e8da452d9
Mark Kerzner <mark@elephantscale.com>
2022-01-06 23:10:48 -0600

    Fixing #309

--- freeeed-processing/src/test/java/org/freeeed/ai/ExtractPiiInabiaTest.java
+++ freeeed-processing/src/test/java/org/freeeed/ai/ExtractPiiInabiaTest.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -20,6 +22,8 @@ import static org.junit.Assert.*;
  * @author mark
  */
 public class ExtractPiiInabiaTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExtractPiiInabiaTest.class);
+
     private String data = "Hello 713-777-7777 Name: John Doe, johndoe@gmail.com. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 1301 McKinney St #2400, Houston, TX 77010";
 
     private String token;
@@ -54,10 +58,9 @@ public class ExtractPiiInabiaTest {
             JSONArray pii = (JSONArray) jsonResponse.get(1);
             for (int i = 0; i < pii.size(); ++i) {
                 JSONObject piiElement = (JSONObject) pii.get(i);
-                System.out.println(piiElement);
             }
         } catch (Exception e) {
-            System.out.println("Exception in NetClientGet:- " + e);
+            LOGGER.error("Exception in NetClientGet:- " + e);
         }
     }
     @Test
@@ -69,18 +72,8 @@ public class ExtractPiiInabiaTest {
     @Test
     public void testExtractPiiMuchoData() {
         ExtractPiiInabia extract = new ExtractPiiInabia(token);
-        String muchoData = data
-                + ". " + data
-                + ". " + data
-                + ". " + data
-                + ". " + data
-                + ". " + data
-                + ". " + data
-                + ". " + data
-                + ". " + data
-                + ". " + data
-                + ". " + data
-                ;
+        String muchoData = "\"***********\n" +
+        "EDRM Enron Email Data Set has been produced in EML, PST and NSF format by ZL Technologies, Inc. This Data Set is licensed under a Creative Commons Attribution 3.0 United States License <http://creativecommons.org/licenses/by/3.0/us/> . To provide attribution, please cite to \"ZL Technologies, Inc. (http://www.zlti.com).\" ***********\"}";
         List<String> result = extract.extractPii(muchoData);
         assertEquals(result.size(), 7);
     }
@@ -89,7 +82,6 @@ public class ExtractPiiInabiaTest {
     public void testExtractPiiAsString() {
         ExtractPiiInabia extract = new ExtractPiiInabia(token);
         String result = extract.extractPiiAsString(data);
-        System.out.println(result);
         assert(result.length() > 0);
     }
 }
