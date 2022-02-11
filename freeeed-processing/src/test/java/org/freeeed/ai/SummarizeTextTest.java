b8a9dc6f76963bf4151217c47e867cedd62f94bd
Mark Kerzner <mark@elephantscale.com>
2022-02-10 23:54:26 -0600

    Fixing #319

--- freeeed-processing/src/test/java/org/freeeed/ai/SummarizeTextTest.java
+++ freeeed-processing/src/test/java/org/freeeed/ai/SummarizeTextTest.java
@@ -20,10 +20,7 @@ import static org.junit.Assert.assertEquals;
 public class SummarizeTextTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(SummarizeTextTest.class);
 
-    private String data = "House Speaker Nancy Pelosi and Democratic leaders have greenlighted a plan to craft legislation " +
-            "that would prohibit members of Congress from trading stock, after months of resistance to a ban by Pelosi, " +
-            "CNBC confirmed Wednesday. At Pelosi's direction, the House Administration Committee is working on drafting the rules, " +
-            "and the legislation is expected to be put up for a vote this year, likely before the November midterm elections.";
+    private String data = "House Speaker Nancy Pelosi and Democratic leaders have greenlighted a plan to craft legislation that would prohibit members of Congress from trading stock, after months of resistance to a ban by Pelosi, CNBC confirmed Wednesday. At Pelosi's direction, the House Administration Committee is working on drafting the rules, and the legislation is expected to be put up for a vote this year, likely before the November midterm elections.";
     private String summary = "Members of Congress could soon be banned from trading on the stock market.";
 
     @Test
