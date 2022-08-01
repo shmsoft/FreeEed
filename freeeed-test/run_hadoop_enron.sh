f0a225bdb8527b871d8b8938e1d1c31b918850eb
Mark Kerzner <mark@elephantscale.com>
2022-07-31 23:02:50 -0500

    removed test folder and project

--- freeeed-test/run_hadoop_enron.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-hadoop fs -rmr /freeeed/output
-hadoop jar \
-target/FreeEed-1.0-SNAPSHOT-jar-with-dependencies.jar org.freeeed.main.MRFreeEedProcess \
--libjars drivers/truezip-driver-zip-7.7.4.jar \
-hadoop_test_s3.project \
-/freeeed/output
--- freeeed-test/run_hadoop_s3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# parameters: 1 - project, 2 - output dir, 3 - number of reducers
-hadoop fs -rmr $2
-hadoop jar \
-target/SHMcloud-1.0-SNAPSHOT-jar-with-dependencies.jar org.freeeed.main.MRFreeEedProcess \
--libjars drivers/truezip-driver-zip-7.7.4.jar -D Xmx3096m -D mapred.reduce.tasks=$3 \
-$1 $2
