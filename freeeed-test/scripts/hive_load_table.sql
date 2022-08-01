34a88674a46ebb86ff16925219d168200a124736
Mark Kerzner <mark@elephantscale.com>
2022-07-31 23:02:50 -0500

    removed test folder and project

--- freeeed-test/scripts/hive_load_table.sql
+++
@@ -1,6 +0,0 @@
--- to run: hive -f hive_load_table.sql
-load data inpath '/freeeed/output/part-r*'
-overwrite into table load_file;
-
--- to verify, enter hive shell, then
--- select count (*) from load_file; 
