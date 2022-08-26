c524343a0fb6352b62077f9213546bc1bef82b28
Mark Kerzner <mark@elephantscale.com>
2022-08-25 22:32:11 -0500

    tested sequential and parallel summarizations

--- freeeed-processing/src/test/java/org/freeeed/ai/SummarizeTextTest.java
+++ freeeed-processing/src/test/java/org/freeeed/ai/SummarizeTextTest.java
@@ -4,24 +4,20 @@
  */
 package org.freeeed.ai;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
 /**
- *
  * @author mark
  */
 public class SummarizeTextTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(SummarizeTextTest.class);
 
-    private String data = "House Speaker Nancy Pelosi and Democratic leaders have greenlighted a plan to craft legislation that would prohibit members of Congress from trading stock, after months of resistance to a ban by Pelosi, CNBC confirmed Wednesday. At Pelosi's direction, the House Administration Committee is working on drafting the rules, and the legislation is expected to be put up for a vote this year, likely before the November midterm elections.";
-    private String summary = "Members of Congress could soon be banned from trading on the stock market.";
+    private final String data = "House Speaker Nancy Pelosi and Democratic leaders have greenlighted a plan to craft legislation that would prohibit members of Congress from trading stock, after months of resistance to a ban by Pelosi, CNBC confirmed Wednesday. At Pelosi's direction, the House Administration Committee is working on drafting the rules, and the legislation is expected to be put up for a vote this year, likely before the November midterm elections.";
+    private final String summary = "Members of Congress could soon be banned from trading on the stock market.";
 
     @Test
     public void testSummarizeText() {
@@ -29,11 +25,27 @@ public class SummarizeTextTest {
         String createdSummary = summarizer.summarizeText(data);
         assertEquals(summary, createdSummary);
     }
+
     @Test
+    // The test below show that one after another, summarizer requests work. But simultaneous requests do not work.
     public void testSummarizeTextModel() {
+        // testing for concurrent users limit
+        int CONCURRENT_USERS = 5;
+        for (int i = 0; i < CONCURRENT_USERS; i++) {
+//            Runnable runnable =
+//                    () -> {
+                oneUnitTest();
+//            };
+//            Thread thread = new Thread(runnable);
+//            thread.start();
+        }
+    }
+
+    private void oneUnitTest() {
         SummarizeText summarizer = new SummarizeText();
         String modelCodeName = "google/pegasus-xsum";
         String createdSummary = summarizer.summarizeText(data, modelCodeName);
         assertEquals(summary, createdSummary);
     }
+
 }
