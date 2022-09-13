27658ee14d42368332111597c2bf8f8850f1f702
Mark Kerzner <mark@elephantscale.com>
2022-09-12 23:43:57 -0500

    voice to text

--- freeeed-processing/src/main/java/org/freeeed/api/RestApiTranscript.java
+++ freeeed-processing/src/main/java/org/freeeed/api/RestApiTranscript.java
@@ -1,15 +1,29 @@
 package org.freeeed.api;
 
+import com.google.gson.Gson;
+
 import java.net.URI;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class RestApiTranscript {
-    public static void main(String[] args) throws Exception {
-        HttpRequest postRequest = HttpRequest.newBuilder()
-                .uri(new URI("https://bit.ly/3yxKEIY"))
-                .header("Authorization", "03f3e46eda8a46e0a00dc5a9a1dc6a3f")
-                .POST(HttpRequest.BodyPublishers.ofString(""))
-                .build();
 
+    public static void main(String[] args) throws Exception {
+        Transcript transcript = new Transcript();
+        transcript.setAudio_url("https://bit.ly/3yxKEIY");
+        Gson gson = new Gson();
+        String jsonRequest = gson.toJson(transcript);
+        System.out.println(jsonRequest);
+        String assemblyAiToken = System.getenv("ASSEMBLYAI_APU_KEY");
+        HttpRequest postRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://api.assemblyai.com/v2/transcript"))
+                .header("Authorization", assemblyAiToken)
+                .POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
+                .build();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse <String> postResponse = client.send(postRequest, BodyHandlers.ofString());
+        System.out.println(postResponse.body());
     }
 }
---
+++ freeeed-processing/src/main/java/org/freeeed/api/Transcript.java
@@ -0,0 +1,14 @@
+package org.freeeed.api;
+
+public class Transcript {
+    public String getAudio_url() {
+        return audio_url;
+    }
+
+    public void setAudio_url(String audio_url) {
+        this.audio_url = audio_url;
+    }
+
+    private String audio_url;
+
+}
