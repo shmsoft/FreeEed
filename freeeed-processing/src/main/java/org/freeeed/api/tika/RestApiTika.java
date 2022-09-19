3de089e71f11d95b5c582bf5f478a80f3aa2175c
Mark Kerzner <mark@elephantscale.com>
2022-09-18 21:46:05 -0500

    tika server and tests

--- freeeed-processing/src/main/java/org/freeeed/api/tika/RestApiTika.java
+++ freeeed-processing/src/main/java/org/freeeed/api/tika/RestApiTika.java
@@ -1,19 +1,10 @@
 package org.freeeed.api.tika;
 
-import com.google.gson.Gson;
 import okhttp3.*;
-import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
+
 
 /**
  * Tika-server REST API implementation
@@ -23,7 +14,11 @@ import java.net.http.HttpResponse.BodyHandlers;
 public class RestApiTika {
     OkHttpClient client = new OkHttpClient();
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
-
+    public static final MediaType MEDIA_TYPE_BINARY
+            = MediaType.parse("application/octet-stream");
+    static String TIKA_URL = "http://localhost:9998/tika";
+    static String METADATA_URL = "http://localhost:9998/meta";
+    static String LANGUAGE_URL = "http://localhost:9998/language/string";
     /**
      * curl -X GET http://localhost:9998/tika
      */
@@ -37,6 +32,7 @@ public class RestApiTika {
         }
     }
 
+
     String postTo(String url, String json) throws IOException {
         RequestBody body = RequestBody.create(json, JSON);
         Request request = new Request.Builder()
@@ -48,49 +44,54 @@ public class RestApiTika {
         }
     }
 
-    String bowlingJson(String player1, String player2) {
-        return "{'winCondition':'HIGH_SCORE',"
-                + "'name':'Bowling',"
-                + "'round':4,"
-                + "'lastSaved':1367702411696,"
-                + "'dateStarted':1367702378785,"
-                + "'players':["
-                + "{'name':'" + player1 + "','history':[10,8,6,7,8],'color':-13388315,'total':39},"
-                + "{'name':'" + player2 + "','history':[6,10,5,10,10],'color':-48060,'total':41}"
-                + "]}";
-    }
-    /*
-    Metadata Resource
-    /meta
-     */
-
-    /**
-     * curl -T price.xls http://localhost:9998/meta
-     */
-    public String getMetadata(String filename) throws Exception {
-        HttpRequest postRequest = HttpRequest.newBuilder()
-                .uri(new URI("http://localhost:9998/meta"))
-                .PUT(HttpRequest.BodyPublishers.ofString(filename))
+    String getLanguage(String json) throws IOException {
+        RequestBody body = RequestBody.create(json, JSON);
+        Request request = new Request.Builder()
+                .url(LANGUAGE_URL)
+                .put(body)
                 .build();
-        HttpClient client = HttpClient.newHttpClient();
-        HttpResponse<String> postResponse = client.send(postRequest, BodyHandlers.ofString());
-        System.out.println(postResponse.body());
-        return postResponse.body();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
     }
 
-    public String getText(String filename) throws Exception {
-        URL url = new URL("http://localhost:9998/tika");
-        HttpURLConnection http = (HttpURLConnection) url.openConnection();
-        http.setRequestMethod("PUT");
-        http.setDoOutput(true);
-        http.setRequestProperty("Content-Type", "text/csv");
-        http.setRequestProperty("Accept", "text/plain");
 
-        byte[] data = FileUtils.readFileToByteArray(new File(filename));
-        OutputStream stream = http.getOutputStream();
-        stream.write(data);
-        String response = stream.toString();
-        http.disconnect();
-        return response;
+
+    String getMetadata(File file) throws IOException {
+        String output = "";
+        Request request = new Request.Builder()
+                .url(METADATA_URL)
+                .put(RequestBody.create(MEDIA_TYPE_BINARY, file))
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            output = response.body().string();
+        }
+        return output;
+    }
+
+
+    String putString(String url, String phrase) throws IOException {
+        RequestBody body = RequestBody.create(phrase, JSON);
+        Request request = new Request.Builder()
+                .url(url)
+                .put(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public String getText(File file) throws Exception {
+        String output = "";
+        Request request = new Request.Builder()
+                .url(TIKA_URL)
+                .put(RequestBody.create(MEDIA_TYPE_BINARY, file))
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            output = response.body().string();
+        }
+        return output;
     }
 }
