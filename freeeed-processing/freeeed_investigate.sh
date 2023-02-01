60cfdcda324c33ee07c4f8efe4ab4c3de65941d9
Mark Kerzner <mark@elephantscale.com>
2023-02-01 14:36:51 -0600

    inestiaget

--- freeeed-processing/freeeed_investigate.sh
+++ freeeed-processing/freeeed_investigate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-java -Xms512m -Xmx1024m -Dlog4j.configuration=file:"config/log4j.properties" \
--cp target/freeeed-processing-1.0-SNAPSHOT-jar-with-dependencies.jar:drivers/truezip-driver-zip-7.7.4.jar \
+java -Xms512m -Xmx1024m \
+-cp target/freeeed-processing-1.0-SNAPSHOT-jar-with-dependencies.jar \
 org.freeeed.piranha.Investigate $1
--- freeeed-processing/src/main/java/org/freeeed/piranha/Investigate.java
+++ freeeed-processing/src/main/java/org/freeeed/piranha/Investigate.java
@@ -1,14 +1,42 @@
 package org.freeeed.piranha;
 
+import org.apache.commons.io.FileUtils;
 import org.freeeed.main.Version;
+import org.freeeed.services.Project;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class Investigate {
-    public static void main(String [] argv) {
+    public static void main(String[] argv) {
         System.out.println("Piranha investigative surgery " + Version.getVersion());
         Investigate instance = new Investigate();
         instance.investigate(argv[0]);
     }
+
     private void investigate(String dir) {
         System.out.println("Investigating " + dir);
+        try {
+            addToInventory(dir);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void addToInventory(String sourceDirectoryName) throws IOException {
+        addDirRecursive(new File(sourceDirectoryName));
+    }
+
+    private void addDirRecursive(File fileOrDir) throws IOException {
+        if (fileOrDir.isFile()) {
+            System.out.println(fileOrDir.getAbsolutePath());
+            Project.getCurrentProject().getSummaryMap().addToSummaryMap(fileOrDir);
+        } else {
+            File[] fileList = fileOrDir.listFiles();
+            for (File file : fileList) {
+                addDirRecursive(file);
+            }
+        }
     }
 }
