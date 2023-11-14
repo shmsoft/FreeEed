29b38580efe8b133ca3187f1fdcef18f769a9f29
Mark Kerzner <mark@elephantscale.com>
2023-11-13 23:33:18 -0600

    Closed #369

--- freeeed-processing/src/main/java/org/freeeed/util/LogFactory.java
+++ freeeed-processing/src/main/java/org/freeeed/util/LogFactory.java
@@ -1,36 +1,57 @@
+/*
+ *
+ * Copyright SHMsoft, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.freeeed.util;
-
 import java.util.logging.*;
 
 public class LogFactory {
-    // Obtain a logger instance from the Logger class
-    private final static Logger LOGGER = LogFactory.getLogger(LogFactory.class.getName());
-
-    public static void main(String[] args) {
-        // Log messages with different importance levels
-        LOGGER.severe("Severe Log");
-        LOGGER.warning("Warning Log");
-        LOGGER.info("Info Log");
-        LOGGER.config("Config Log");
-        LOGGER.fine("Fine Log");
-        LOGGER.finer("Finer Log");
-        LOGGER.finest("Finest Log");
+    public static final String LOG_FILE_DIR = "logs/";
+    public static final String LOG_FILE_NAME = "FreeEed.log";
+    private static Logger logger = null;
+    /**
+     * Get a logger for the given class.
+     * @param name not used
+     * @return logger fpr everyone
+     */
+    public static Logger getLogger(String name) {
+        if (logger == null) {
+            synchronized (LogFactory.class) {
+                if (logger == null) {
+                    logger = Logger.getLogger("FreeEed");
+                    configureLogger(logger);
+                }
+            }
+        }
+        return logger;
     }
-    public static Logger getLogger(String className) {
-        Logger myLogger = Logger.getLogger(className);
-        myLogger.setLevel(Level.ALL);
-        myLogger.setUseParentHandlers(false); // Disable parent handlers
+
+    private static void configureLogger(Logger logger) {
+        logger.setLevel(Level.ALL);
+        logger.setUseParentHandlers(false);
+
         ConsoleHandler consoleHandler = new ConsoleHandler();
         consoleHandler.setLevel(Level.ALL);
-        myLogger.addHandler(consoleHandler);
+        logger.addHandler(consoleHandler);
+
         try {
-            FileHandler fileHandler = new FileHandler("logs/freeeed.log", true);
+            FileHandler fileHandler = new FileHandler(LOG_FILE_DIR + LOG_FILE_NAME, 0, 1, true);
             fileHandler.setFormatter(new SimpleFormatter());
-            myLogger.addHandler(fileHandler);
-        }
-        catch (Exception e) {
+            logger.addHandler(fileHandler);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return myLogger;
     }
 }
