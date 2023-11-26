992d0f187274271f7baa4839c7b0f6b22c641bb7
Mark Kerzner <mark@elephantscale.com>
2023-11-26 15:49:41 -0600

    working on #379

--- freeeed-processing/src/main/java/org/freeeed/api/transcribe/RestApiTranscript.java
+++ freeeed-processing/src/main/java/org/freeeed/api/transcribe/RestApiTranscript.java
@@ -8,45 +8,101 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
+import okhttp3.*;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * REST API code and text-to-speech courtesy of https://www.youtube.com/watch?v=9oq7Y8n1t00
  */
 
 public class RestApiTranscript {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Currently, this is not part of FreeEed and scheduled to be archived.");
-        System.exit(9);
+    public static void main(String[] args)  {
+        String url = "https://bit.ly/3yxKEIY";
+        RestApiTranscript restApiTranscript = new RestApiTranscript();
+        String transcript = restApiTranscript.getTranscriptionFromUrl(url);
+        System.out.println(transcript);
+    }
+    public String getTranscriptionFromFile(String filename) {
+        String url = uploadToTheCloud(filename);
+        RestApiTranscript restApiTranscript = new RestApiTranscript();
+        String transcript = restApiTranscript.getTranscriptionFromUrl(url);
+        System.out.println(transcript);
+        return transcript;
+    }
+
+    public String uploadToTheCloud(String filename) {
+        // upload file filename to S3 cloud
+
+        String url = "";
+        return url;
+    }
+    public String getTranscriptionFromUrl(String url) {
         Transcript transcript = new Transcript();
-        transcript.setAudio_url("https://bit.ly/3yxKEIY");
+        transcript.setAudio_url(url);
         Gson gson = new Gson();
         String jsonRequest = gson.toJson(transcript);
         System.out.println(jsonRequest);
         String assemblyAiToken = System.getenv("ASSEMBLYAI_API_KEY");
-        HttpRequest postRequest = HttpRequest.newBuilder()
-                .uri(new URI("https://api.assemblyai.com/v2/transcript"))
-                .header("Authorization", assemblyAiToken)
-                .POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
-                .build();
-        HttpClient client = HttpClient.newHttpClient();
-        HttpResponse <String> postResponse = client.send(postRequest, BodyHandlers.ofString());
-        System.out.println(postResponse.body());
-        transcript = gson.fromJson(postResponse.body(), Transcript.class);
-        System.out.println(transcript.getId());
-        HttpRequest getRequest = HttpRequest.newBuilder()
-                .uri(new URI("https://api.assemblyai.com/v2/transcript/" + transcript.getId()))
-                .header("Authorization", assemblyAiToken)
-                .build();
-        while(true) {
-            HttpResponse<String> getResponse = client.send(getRequest, BodyHandlers.ofString());
-            transcript = gson.fromJson(getResponse.body(), Transcript.class);
-            System.out.println(transcript.getStatus());
-            if ("completed".equals(transcript.getStatus()) || "error".equals(transcript.getStatus())) {
-                break;
+        try {
+            assemblyAiToken = System.getenv("ASSEMBLYAI_API_KEY");
+
+            HttpRequest postRequest = HttpRequest.newBuilder()
+                    .uri(new URI("https://api.assemblyai.com/v2/transcript"))
+                    .header("Authorization", assemblyAiToken)
+                    .POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
+                    .build();
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse <String> postResponse = client.send(postRequest, BodyHandlers.ofString());
+            System.out.println(postResponse.body());
+            transcript = gson.fromJson(postResponse.body(), Transcript.class);
+            System.out.println(transcript.getId());
+            HttpRequest getRequest = HttpRequest.newBuilder()
+                    .uri(new URI("https://api.assemblyai.com/v2/transcript/" + transcript.getId()))
+                    .header("Authorization", assemblyAiToken)
+                    .build();
+            while(true) {
+                HttpResponse<String> getResponse = client.send(getRequest, BodyHandlers.ofString());
+                transcript = gson.fromJson(getResponse.body(), Transcript.class);
+                System.out.println(transcript.getStatus());
+                if ("completed".equals(transcript.getStatus()) || "error".equals(transcript.getStatus())) {
+                    break;
+                }
+                Thread.sleep(1000);
             }
-            Thread.sleep(1000);
+            System.out.println("Transcription completed");
+        } catch (Exception e) {
+            System.out.println("Environment variable ASSEMBLYAI_API_KEY not set");
+            System.exit(1);
         }
-        System.out.println("Transcription completed");
-        System.out.println(transcript.getText());
+        return transcript.getText();
+    }
+
+    public String transcribeDirectly(String filePath) throws IOException {
+        String responseBody = "";
+        OkHttpClient httpClient = new OkHttpClient();
+        String apiKey = System.getenv("ASSEMBLYAI_API_KEY");
+        MediaType mediaType = MediaType.parse("audio/mpeg"); // Change this according to your audio file type
+        File audioFile = new File(filePath);
+        RequestBody requestBody = RequestBody.create(audioFile, mediaType);
+
+        Request request = new Request.Builder()
+                .url("https://api.assemblyai.com/v2/transcript")
+                .post(requestBody)
+                .addHeader("authorization", apiKey)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            // Process the response here
+            responseBody = response.body().string();
+            // You can parse this response to get the transcription ID and then poll for the result
+            System.out.println(responseBody);
+        }
+        return responseBody;
     }
 }
--- freeeed-processing/src/test/java/org/freeeed/api/transcribe/RestApiTranscribeTest.java
+++ freeeed-processing/src/test/java/org/freeeed/api/transcribe/RestApiTranscribeTest.java
@@ -19,10 +19,20 @@ public class RestApiTranscribeTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(RestApiTranscribeTest.class);
 
 
-    @Test
-    public void testApplication() throws Exception {
+    //@Test
+    public void testTranscriptionFromUrl() throws Exception {
+        String url = "https://bit.ly/3yxKEIY";
+        RestApiTranscript restApiTranscript = new RestApiTranscript();
+        String transcript = restApiTranscript.getTranscriptionFromUrl(url);
+        System.out.println(transcript);
         assertTrue(true);
-
     }
-
+    @Test
+    public void testTranscribeDirectly() throws Exception {
+        String filename = "test-data/06-audio/7510.mp3";
+        RestApiTranscript restApiTranscript = new RestApiTranscript();
+        String transcript = restApiTranscript.transcribeDirectly(filename);
+        System.out.println(transcript);
+        assertTrue(true);
+    }
 }
