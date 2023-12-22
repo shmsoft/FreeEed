8ebc0398dcbe22d5bb35f7453a09d85c96b683d8
Mark Kerzner <mark@elephantscale.com>
2023-12-22 17:14:19 -0600

    reliase

--- start_dev_services.sh
+++ start_dev_services.sh
@@ -8,10 +8,10 @@ unset CATALINA_HOME
 unset CATALINA_BASE
 chmod -R 755 freeeed-tomcat
 # fix for Mac
-chmod u+x freeeed-tomcat/bin/*.sh
-cd freeeed-tomcat/bin;
-./startup.sh &
-cd ../..
+# chmod u+x freeeed-tomcat/bin/*.sh
+#cd freeeed-tomcat/bin;
+#./startup.sh &
+#cd ../..
 
 cd freeeed-solr/example
 java -Xmx1024M -jar start.jar &
