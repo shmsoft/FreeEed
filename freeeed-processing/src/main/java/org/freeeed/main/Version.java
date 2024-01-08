3bff67fd55277f891616025ffb42ba30a83a4b75
Mark Kerzner <mark@elephantscale.com>
2024-01-07 21:21:40 -0600

    10.0.18

--- freeeed-processing/src/main/java/org/freeeed/main/Version.java
+++ freeeed-processing/src/main/java/org/freeeed/main/Version.java
@@ -27,7 +27,7 @@ import java.util.Date;
 public class Version {
     // This version shows in the About dialog
     // There is also a VersionNumber.txt file under freeeed-processing
-    private static final String V = "10.0.17";
+    private static final String V = "10.0.18";
     public static String getVersionAndBuild() {
         return ParameterProcessing.APP_NAME + " " + getVersionNumber()  + ". Build date: " + getBuildTime();
     }       
