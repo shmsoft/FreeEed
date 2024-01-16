827a7d016ffeed552bf0ccfce2590ac6bc8b79fd
Mark Kerzner <mark@elephantscale.com>
2024-01-15 22:58:36 -0600

    upgrade script

--- upgrade_freeeed.sh
+++ upgrade_freeeed.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-cd 10.1.0
+cd 10.1.1
 if [ -z "$1" ]; then
     echo "No IP provided."
     exit 1
 fi
-scp freeeed_complete_pack/freeeed-tomcat/webapps/freeeedui.war ubuntu@3.144.38.131:/home/ubuntu/Desktop/freeeed_complete_pack/freeeed-tomcat/webapps/freeeedui.war
-scp freeeed_complete_pack/FreeEed/target/freeeed-processing-1.0-SNAPSHOT-jar-with-dependencies.jar ubuntu@3.144.38.131:/home/ubuntu/Desktop/freeeed_complete_pack/FreeEed/target/freeeed-processing-1.0-SNAPSHOT-jar-with-dependencies.jar
+scp freeeed_complete_pack/freeeed-tomcat/webapps/freeeedui.war ubuntu:@1:/home/ubuntu/Desktop/freeeed_complete_pack/freeeed-tomcat/webapps/freeeedui.war
+scp freeeed_complete_pack/FreeEed/target/freeeed-processing-1.0-SNAPSHOT-jar-with-dependencies.jar ubuntu:@1:/home/ubuntu/Desktop/freeeed_complete_pack/FreeEed/target/freeeed-processing-1.0-SNAPSHOT-jar-with-dependencies.jar
