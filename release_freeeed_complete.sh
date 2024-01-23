351127964f7bf5c5afba3486b53721427cc1fdc6
Mark Kerzner <mark@elephantscale.com>
2024-01-23 15:31:57 -0600

    10.1.4

--- release_freeeed_complete.sh
+++ release_freeeed_complete.sh
@@ -17,7 +17,7 @@ PROJECT_DIR=$SHMSOFT_HOME
 RELEASE_DIR=$PROJECT_DIR/release
 FREEEED_PROJECT=$PROJECT_DIR/FreeEed
 FREEEED_UI_PROJECT=$PROJECT_DIR/FreeEedUI
-VERSION=10.1.3
+VERSION=10.1.4
 echo "Building version "$VERSION
 
 #============================ user setup ==================================
