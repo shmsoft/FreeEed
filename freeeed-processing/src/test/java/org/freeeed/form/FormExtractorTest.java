505a87fe063f4ac6d9b47906ea4041925b2e7d47
Mark Kerzner <mark@elephantscale.com>
2024-04-04 23:02:11 -0500

    forms

--- freeeed-processing/src/test/java/org/freeeed/form/FormExtractorTest.java
+++ freeeed-processing/src/test/java/org/freeeed/form/FormExtractorTest.java
@@ -3,7 +3,6 @@ package org.freeeed.form;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class FormExtractorTest {
