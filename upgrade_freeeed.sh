45cc8f35c24b1f9c39b2f64d40aaf8095824068d
Mark Kerzner <mark@elephantscale.com>
2024-04-09 00:55:33 -0500

    SKY upgrade

--- upgrade_freeeed.sh
+++ upgrade_freeeed.sh
@@ -1,9 +1,9 @@
-# This has to be run from the new release
-# Like `cd 10.2.6`
+# Upgrade SKY VM
 #!/usr/bin/env bash
 if [ -z "$1" ]; then
     echo "No IP provided."
     exit 1
 fi
-scp freeeed_complete_pack/freeeed-tomcat/webapps/freeeedui.war ubuntu:$1:/home/ubuntu/Desktop/freeeed_complete_pack/freeeed-tomcat/webapps/freeeedui.war
-scp freeeed_complete_pack/FreeEed/target/freeeed-processing-1.0-SNAPSHOT-jar-with-dependencies.jar ubuntu:$1:/home/ubuntu/Desktop/freeeed_complete_pack/FreeEed/target/freeeed-processing-1.0-SNAPSHOT-jar-with-dependencies.jar
+scp -i ~/.ssh/freeeed_admin.pem ~/projects/SHMsoft/freeeed_complete_pack/freeeed-tomcat/webapps/freeeedui.war freeeed@$1:/home/freeeed/Desktop/freeeed_complete_pack/freeeed-tomcat/webapps/
+scp -i ~/.ssh/freeeed_admin.pem ~/projects/SHMsoft/freeeed_complete_pack/FreeEed/target/freeeed-processing-1.0-SNAPSHOT-jar-with-dependencies.jar freeeed@$1:/home/freeeed/Desktop/freeeed_complete_pack/FreeEed/target/freeeed-processing-1.0-SNAPSHOT-jar-with-dependencies.jar
+scp -i ~/.ssh/freeeed_admin.pem  ~/projects/scaia/AIAdvisor/code/python/* freeeed@$1:~/projects/scaia/AIAdvisor/code/python/
