4eb9a3e5702fb4d69eb38e6a589d865487a41ac2
richa <nehadubey0605@gmail.com>
2024-04-12 14:22:11 +0400

    added test for tika server and increased the timeout to be 5 minutes for read and write

---
+++ freeeed-processing/src/test/java/org/freeeed/api/tika/TikaRestApiTest.java
@@ -0,0 +1,31 @@
+package org.freeeed.api.tika;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/*
+ *@created 12/04/2024- 13:53
+ *@author neha
+ */public class TikaRestApiTest extends TestCase {
+
+    public void testGetText() throws Exception {
+        String fileName = "aluminum.pdf";
+        ClassLoader classLoader = getClass().getClassLoader();
+        assertNotNull(classLoader.getResource(fileName));
+
+        Path resourcePath = Paths.get(classLoader.getResource(fileName).toURI());
+
+        File file = resourcePath.toFile();
+        TikaRestApi tikaRestApi = new TikaRestApi();
+        String text = tikaRestApi.getText(file);
+        assertNotNull(text);
+        assertTrue(text.contains("ommission staff and other government officials have investigated\n" +
+                "numerous compiaints from homeowners"));
+        assertTrue(text.contains("TWIST-ON\n" +
+                "PRESSURE WIRE\n" +
+                "CONNECTOR"));
+    }
+}
