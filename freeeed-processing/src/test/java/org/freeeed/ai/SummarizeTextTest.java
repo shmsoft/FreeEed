8200b879ba95fb78c3691e275a45239113bc5f75
Mark Kerzner <mark@scaia.ai>
2024-04-16 00:49:57 -0500

    WIP

--- freeeed-processing/src/test/java/org/freeeed/ai/SummarizeTextTest.java
+++ freeeed-processing/src/test/java/org/freeeed/ai/SummarizeTextTest.java
@@ -5,7 +5,7 @@
 package org.freeeed.ai;
 
 import org.junit.Test;
-import org.slf4j.Logger;
+import org.freeeed.util.LogFactory;
 import org.slf4j.LoggerFactory;
 
 import static org.junit.Assert.assertEquals;
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertEquals;
  * @author mark
  */
 public class SummarizeTextTest {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SummarizeTextTest.class);
+    private final static java.util.logging.Logger LOGGER = LogFactory.getLogger(SummarizeTextTest.class.getName());
 
     private final String data = "House Speaker Nancy Pelosi and Democratic leaders have greenlighted a plan to craft legislation that would prohibit members of Congress from trading stock, after months of resistance to a ban by Pelosi, CNBC confirmed Wednesday. At Pelosi's direction, the House Administration Committee is working on drafting the rules, and the legislation is expected to be put up for a vote this year, likely before the November midterm elections.";
     private final String summary = "Members of Congress could soon be banned from trading on the stock market.";
