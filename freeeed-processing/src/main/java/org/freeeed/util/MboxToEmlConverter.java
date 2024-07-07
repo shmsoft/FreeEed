d60b903408a6f0e34617195edfcc26a996c9512b
Neha <nehadubey0605@gmail.com>
2024-07-07 10:38:41 +0400

    added mbox processor, tested it manually

---
+++ freeeed-processing/src/main/java/org/freeeed/util/MboxToEmlConverter.java
@@ -0,0 +1,63 @@
+package org.freeeed.util;/*
+ *@created 06/07/2024- 21:04
+ *@author neha
+ */
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class MboxToEmlConverter {
+
+    public static List<String> convertMboxToEml(String mboxFilePath, String outputDirPath) throws IOException {
+        List<String> extractedFilePaths = new LinkedList<>();
+        File mboxFile = new File(mboxFilePath);
+        if (!mboxFile.exists() || !mboxFile.isFile()) {
+            throw new FileNotFoundException("Invalid MBOX file path provided.");
+        }
+
+        Path outputDir = Paths.get(outputDirPath);
+        if (!Files.exists(outputDir)) {
+            Files.createDirectories(outputDir);
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(mboxFile))) {
+            String curLine = reader.readLine();
+            int mailItem = 0;
+
+            while (curLine != null) {
+                if (curLine.startsWith("From ")) {
+                    Queue<String> multiline = new LinkedList<>();
+                    File emlFile = new File(outputDir.toFile(), mboxFilePath.substring(mboxFilePath.lastIndexOf("/"))+"_email_" + mailItem + ".eml");
+
+                    try (BufferedWriter emlWriter = new BufferedWriter(new FileWriter(emlFile))) {
+                        do {
+                            if (!curLine.startsWith(" ") && !curLine.startsWith("\t")) {
+                                multiline.add(curLine);
+                            } else {
+                                String latestLine = multiline.poll();
+                                latestLine = latestLine + " " + curLine.trim();
+                                multiline.add(latestLine);
+                            }
+
+                            emlWriter.write(curLine);
+                            emlWriter.newLine();
+                            curLine = reader.readLine();
+                        } while (curLine != null && !curLine.startsWith("From "));
+
+                        System.out.println("Email saved to: " + emlFile.getAbsolutePath());
+                        extractedFilePaths.add(emlFile.getAbsolutePath());
+                    }
+
+                    mailItem++;
+                } else {
+                    curLine = reader.readLine();
+                }
+            }
+        }
+        return extractedFilePaths;
+    }
+}
