456a25d0dd771044ae0cc14d724a71138fc5cbf2
Mark Kerzner <mark@elephantscale.com>
2024-12-10 20:40:51 -0600

    fixes

---
+++ freeeed-processing/src/main/java/org/freeeed/main/InvestigatorProcessor.java
@@ -0,0 +1,74 @@
+/*
+ *
+ * Copyright SHMsoft, Inc. 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+package org.freeeed.main;
+
+import org.freeeed.data.index.LuceneIndex;
+import org.freeeed.mr.MetadataWriter;
+import org.freeeed.services.Settings;
+import org.freeeed.services.Util;
+import org.freeeed.util.LogFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+
+/**
+ * Investigate file for responsiveness to a query
+ */
+public class InvestigatorProcessor extends FileProcessor {
+    private final static Logger LOGGER = LogFactory.getLogger(InvestigatorProcessor.class.getName());
+
+    /**
+     * Constructor
+     *
+     * @param singleFileName
+     * @param metadataWriter
+     * @param luceneIndex
+     */
+    public InvestigatorProcessor(String singleFileName, MetadataWriter metadataWriter, LuceneIndex luceneIndex) {
+        super(metadataWriter, luceneIndex);        
+        this.singleFileName = singleFileName;
+    }
+
+    /**
+     * Process file
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Override
+    public void process(boolean hasAttachments, String hash) throws IOException, InterruptedException {
+        String emailPath = getSingleFileName();
+        String emailName = new File(emailPath).getName();
+        // TODO this is a little more complex, there are attachments without extensions
+        // if the file already has an extension - then it is an attachment
+        String ext = Util.getExtension(emailName);
+        if (ext.isEmpty()) {
+            emailName += ".eml";
+        }
+        
+        LOGGER.fine("Processing eml file with path: " + emailPath + ", name: " + emailName);
+        processFileEntry(new DiscoveryFile(emailPath, emailName, hasAttachments, hash));
+    }
+
+    @Override
+    String getOriginalDocumentPath(DiscoveryFile discoveryFile) {
+        String pathToEmail = discoveryFile.getPath().getPath().substring(Settings.getSettings().getPSTDir().length() + 1);
+        return new File(pathToEmail).getParent() + File.separator + discoveryFile.getRealFileName();
+    }
+}
