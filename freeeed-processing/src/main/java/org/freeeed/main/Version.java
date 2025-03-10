56095c67b98942b77d7a2e2681bb30963d3374ca
Mark Kerzner <mark@elephantscale.com>
2025-03-10 00:01:57 -0500

    non-existent processing engine should stop processing

--- freeeed-processing/src/main/java/org/freeeed/main/Version.java
+++ freeeed-processing/src/main/java/org/freeeed/main/Version.java
@@ -26,7 +26,7 @@ import java.util.Date;
  */
 public class Version {
     // This version shows in the About dialog
-    private static final String V = "10.3.2";
+    private static final String V = "10.4.0";
     public static String getVersionAndBuild() {
         return ParameterProcessing.APP_NAME + " " + getVersionNumber()  + ". Build date: " + getBuildTime();
     }       
