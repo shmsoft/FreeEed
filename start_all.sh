0ac74c1ca03ee9f3826108b8b85bb8a635834ca9
Mark Kerzner <mark@elephantscale.com>
2025-11-30 22:43:35 -0600

    cleaned start_all.sh

--- start_all.sh
+++ start_all.sh
@@ -1,26 +1,24 @@
-echo off
+#!/bin/bash
+# this script should be run from freeeed_complete_pack
+echo "******************** Starting FreeEed development services"
 
 unset CATALINA_HOME
 unset CATALINA_BASE
+
 chmod -R 755 freeeed-tomcat
-# fix for Mac
-chmod u+x freeeed-tomcat/bin/*.sh
-cd freeeed-tomcat/bin;
-./startup.sh &
-cd ../..
 
+# Ensure logs directory exists
+mkdir -p logs
+
+echo "Starting Solr..."
 cd freeeed-solr/example
-java -Xmx1024M -jar start.jar &
+nohup java -Xmx1024M -jar start.jar > ../../logs/solr.log 2>&1 &
 cd ../..
 
-ulimit -Sn 4000
+echo "Starting Tika..."
 cd freeeed-tika
-java -Xmx1024M -jar tika-server.jar &
+nohup java -Xmx1024M -jar tika-server.jar > ../logs/tika.log 2>&1 &
 cd ..
 
+echo "All services started. Logs are in ./logs/"
 
-cd FreeEed
-./freeeed_player.sh &
-
-cd ~/projects/scaia/AIAdvisor/code/python
-uvicorn  main:app --reload --host 0.0.0.0 &
